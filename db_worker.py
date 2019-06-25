c052d40524e7b20aec2d71e7aa13a99f7230ba90
Vladimir <48794941+1a11@users.noreply.github.com>
2019-06-25 17:27:39 +0400

    db_worker.py
    PEP8 update

--- db_worker.py
+++ db_worker.py
@@ -1,6 +1,8 @@
 import sqlite3
+
+
 class database():
-    def __init__(self, dbname):
+    def __init__(self,  dbname):
         try:
             global conn
             conn = sqlite3.connect(dbname)
@@ -10,17 +12,17 @@ class database():
             raise Exception(exc)
     #--<
     """
-        If your database was f***ed up by something, you can create new one
+        If your database was f***ed up by something,  you can create new one
 
         create() - will create a new tables in your database
     """
     def create(self):
         cursor.execute("""CREATE TABLE settings
-                  (channel text, emoji text, copypasta text,
-                   troll text, insult text, alt text)
+                  (channel text,  emoji text,  copypasta text, 
+                   troll text,  insult text,  alt text)
                """)
         cursor.execute("""CREATE TABLE coefs
-                  (channel text, insult real, troll real)
+                  (channel text,  insult real,  troll real)
                """)
         
     #--<
@@ -28,48 +30,50 @@ class database():
         Get colum values
         
         get_settings(channel) - will get values from table settings where channel is channel name
-            channel - type: string, example: '#abcdef'
+            channel - type: string,  example: '#abcdef'
 
         get_coefs(channel) - will get values from table coefs where channel is channel name
-            channel - type: string, example: '#abcdef'
+            channel - type: string,  example: '#abcdef'
     """       
-    def get_settings(self, channel):
+    def get_settings(self,  channel):
         sql = "SELECT * FROM settings WHERE channel=?"
-        cursor.execute(sql, [(channel)])
+        cursor.execute(sql,  [(channel)])
         return cursor.fetchall()
-    def get_coefs(self, channel):
+    
+    def get_coefs(self,  channel):
         sql = "SELECT * FROM coefs WHERE channel=?"
-        cursor.execute(sql, [(channel)])
+        cursor.execute(sql,  [(channel)])
         return cursor.fetchall()
     
     #--<
     """
         Creating colums for values
         
-        create_settings(channel, values) - will create new settings colum in table settings
-            channel - type: string, example: '#abcdef'
-            values - type: dict, example: {'emoji':'y','copypasta':'y','troll':'y','insult':'y','alt':'y'}
+        create_settings(channel,  values) - will create new settings colum in table settings
+            channel - type: string,  example: '#abcdef'
+            values - type: dict,  example: {'emoji':'y', 'copypasta':'y', 'troll':'y', 'insult':'y', 'alt':'y'}
 
-        create_coefs(channel, values) - will create new coefs colum in table coefs
-            channel - type: string, example: '#abcdef'
-            values - type: dict, example: {'troll':0.96,'insult':0.47}
+        create_coefs(channel,  values) - will create new coefs colum in table coefs
+            channel - type: string,  example: '#abcdef'
+            values - type: dict,  example: {'troll':0.96, 'insult':0.47}
     """
     
-    def create_settings(self, channel, values):
+    def create_settings(self,  channel,  values):
         sql = """
         INSERT INTO settings
-        VALUES ('{}','{}','{}',
-                '{}','{}','{}')
-        """.format(channel,values['emoji'],values['copypasta'],
-                   values['troll'],values['insult'],values['alt'])
+        VALUES ('{}', '{}', '{}', 
+                '{}', '{}', '{}')
+        """.format(channel, values['emoji'], values['copypasta'], 
+                   values['troll'], values['insult'], values['alt'])
         print(sql)
         cursor.execute(sql)
         conn.commit()
-    def create_coefs(self, channel, values):
+        
+    def create_coefs(self,  channel,  values):
         sql = """
         INSERT INTO coefs
-        VALUES ('{}','{}','{}')
-        """.format(channel, values['troll'], values['insult'])
+        VALUES ('{}', '{}', '{}')
+        """.format(channel,  values['troll'],  values['insult'])
         print(sql)
         cursor.execute(sql)
         conn.commit()
@@ -78,33 +82,34 @@ class database():
     """
         Updating colums values
         
-        set_settings(channel, values) - will update values in table settings where channel is channel name and values are y/n (yes/no)
-            channel - type: string, example: '#abcdef'
-            values - type: dict, example: {'emoji':'y','copypasta':'y','troll':'y','insult':'y','alt':'y'}
+        set_settings(channel,  values) - will update values in table settings where channel is channel name and values are y/n (yes/no)
+            channel - type: string,  example: '#abcdef'
+            values - type: dict,  example: {'emoji':'y', 'copypasta':'y', 'troll':'y', 'insult':'y', 'alt':'y'}
 
         set_coefs(channel_values) - will update values in table coefs where channel is channel name and values are nn coefs
-            channel - type: string, example: '#abcdef'
-            values - type: dict, example: {'emoji':0.3,'copypasta':0.50,'troll':0.96,'insult':0.47,'alt':0.35}
+            channel - type: string,  example: '#abcdef'
+            values - type: dict,  example: {'emoji':0.3, 'copypasta':0.50, 'troll':0.96, 'insult':0.47, 'alt':0.35}
     """    
-    def set_settings(self, channel, values):
+    def set_settings(self,  channel,  values):
         sql = """
         UPDATE settings
-        SET emoji = '{}',
-            copypasta = '{}',
-            troll = '{}',
-            insult = '{}',
+        SET emoji = '{}', 
+            copypasta = '{}', 
+            troll = '{}', 
+            insult = '{}', 
             alt = '{}'
         WHERE channel = '{}'
-        """.format(values['emoji'],values['copypasta'],
-                   values['troll'],values['insult'],values['alt'], channel)
+        """.format(values['emoji'], values['copypasta'], 
+                   values['troll'], values['insult'], values['alt'],  channel)
         cursor.execute(sql)
         conn.commit()
-    def set_coefs(self, channel, values):
+        
+    def set_coefs(self,  channel,  values):
         sql = """
         UPDATE settings
-        SET troll = '{}',
+        SET troll = '{}', 
             insult = '{}'
         WHERE channel = '{}'
-        """.format(values['troll'],values['insult'], channel)
+        """.format(values['troll'], values['insult'],  channel)
         cursor.execute(sql)
         conn.commit()
